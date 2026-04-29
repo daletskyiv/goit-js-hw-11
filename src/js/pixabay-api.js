19b83ecaa32f82d7e8b1604270c8661d460b20ca
daletskyi_v <daletskyiv@gmail.com>
2026-04-29 18:22:28 +0300

    add basic markup to html, css and js files

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export function getImagesByQuery(query) {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '55643429-294578a097483c19c7f77277b',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: '21',
+    },
+  });
+}
---
+++ src/main.js
@@ -0,0 +1,59 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-function';
+
+export const refs = {
+  searchForm: document.querySelector('.form'),
+  galleryList: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+
+  const userQuery = event.target.elements.search_text.value.trim();
+
+  if (!userQuery) {
+    iziToast.error({
+      message: 'The search field must be filled in!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+
+  showLoader();
+
+  getImagesByQuery(userQuery)
+    .then(response => {
+      if (response.data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(response.data.hits);
+    })
+    .catch(err => {
+      console.log(err);
+
+      iziToast.error({
+        message: `${err}`,
+        position: 'topRight',
+      });
+    })
+    .finally(() => hideLoader());
+
+  refs.searchForm.reset();
+}
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
